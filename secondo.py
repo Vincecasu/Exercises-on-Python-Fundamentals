a26f19b32834f0a84c6d8f70ad452aac01237395
Vincecasu <vincenzocasuccio77@gmail.com>
2024-02-20 11:44:44 +0100

    fff

---
+++ secondo.py
@@ -0,0 +1,15 @@
+i1 = 2
+i2 = 3
+s1 =  "giorno"
+s2 =  "notte"
+f1 = 1.2
+f2 = 1.5
+b1 = True
+b2 = False
+
+print (i1+i2)
+print (s1 +s2)
+print (f1+f2)
+print (b1+b2)
+print (i1+s1)
+print (i1+f2)
